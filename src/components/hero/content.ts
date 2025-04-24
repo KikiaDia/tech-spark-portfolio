d3d4a1572b127a586deba7ed014107553ce69a02
KikiaDia <104317306+KikiaDia@users.noreply.github.com>
2025-04-24 11:35:02 +0000

    update project

--- src/components/hero/content.ts
+++ src/components/hero/content.ts
@@ -2,9 +2,9 @@ export const content = {
   en: {
     quote: "Turning Data into Insights, Code into Innovation",
     role: "Computer Engineer",
-    description: "I am Kikia Dia, a Computer Engineering graduate, specializing in Machine Learning and NLP, with a strong passion for data. I am open to opportunities in AI development, data analysis, and problem-solving, aiming to create impactful solutions.",
+    description: "I am Kikia Dia, a Computer Engineering graduate, specializing in Machine Learning and NLP, with a strong passion for data and generative AI. I am open to opportunities in AI development aiming to create impactful solutions.",
     details: {
-      age: "24 years old",
+      age: "25 years old",
       status: "Single",
       license: "Driving License (A1, B)",
       address: "Nantes, France",
@@ -19,9 +19,9 @@ export const content = {
   fr: {
     quote: "Transformer les Données en Insights, le Code en Innovation",
     role: "Ingénieure Informatique",
-    description: "Je suis Kikia Dia, diplômée en Ingénierie Informatique, spécialisée en Machine Learning et NLP, avec une grande passion pour les données. Je suis ouverte aux opportunités en développement IA, analyse de données et résolution de problèmes pour créer des solutions à fort impact.",
+    description: "Je suis Kikia Dia, diplômée en Ingénierie Informatique, spécialisée en Machine Learning et NLP, avec une grande passion pour les données et l'IA générative. Je suis ouverte aux opportunités en développement IA pour créer des solutions à fort impact.",
     details: {
-      age: "24 ans",
+      age: "25 ans",
       status: "Célibataire",
       license: "Permis (A1, B)",
       address: "Nantes, France",
