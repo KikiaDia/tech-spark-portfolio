e78480997827d896c201d51a4ccfbcc642438715
KikiaDia <104317306+KikiaDia@users.noreply.github.com>
2026-01-16 20:25:48 +0000

    update

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -8,13 +8,6 @@ export const projects = {
       github: "https://github.com/KikiaDia/chatbot_agricole",
       videoUrl: "https://drive.google.com/file/d/1WjI0a3tap1j02RL4FG1sSCZayJK8j-Bt/preview"
     },
-        {
-      title: "Codeforces Multilabel Classification",
-      description: "Multi-label classification of programming problems from Codeforces, predicting algorithmic tags using ML models and code features.",
-      banner: "/lovable-uploads/codeforces.png",
-      tags: ["Machine Learning", "Multi-label", "Python", "TF-IDF", "LinearSVC", "Transformers", "EDA"],
-      github: "https://github.com/KikiaDia/multilabel_code_classification.git"
-    },
     {
       title: "HR Dashboard – Q3 2024",
       description: "Interactive Python dashboard to analyze HR data for Q3 2024, consolidating KPIs, visualizing monthly trends, and querying data with a conversational agent.",
@@ -22,6 +15,20 @@ export const projects = {
       tags: ["Dash", "Plotly", "pandas", "LangChain", "GPT-4", "Pyngrok"],
       github: "https://github.com/KikiaDia/Dashboard-RH.git",
       videoUrl :"https://drive.google.com/file/d/1Qb-79WZH8c_pjQgg7Sejs5djHRzsQrsE/preview"
+    },
+    {
+      title: "Codeforces Multilabel Classification",
+      description: "Multi-label classification of programming problems from Codeforces, predicting algorithmic tags using ML models and code features.",
+      banner: "/lovable-uploads/codeforces.png",
+      tags: ["Machine Learning", "Multi-label", "Python", "TF-IDF", "LinearSVC", "Transformers", "EDA"],
+      github: "https://github.com/KikiaDia/multilabel_code_classification.git"
+    },
+    {
+      title: "Disaster Detection in Tweets",
+      description: "Development, optimization, and deployment of machine learning models to classify tweets into 'disaster' and 'non-disaster' categories.",
+      banner: "/lovable-uploads/twitter.png",
+      tags: ["Machine Learning", "Deep Learning", "MLFlow", "EDA"],
+      github: "https://github.com/dnaby/NLP-Disaster-Tweets-Detection?tab=readme-ov-file",
     },
     {
       title: "Fintech Fraud Detection",
@@ -40,13 +47,6 @@ export const projects = {
       github: "https://github.com/KikiaDia/Real-time-Weather-Pipeline/tree/main",
     },
     {
-      title: "Disaster Detection in Tweets",
-      description: "Development, optimization, and deployment of machine learning models to classify tweets into 'disaster' and 'non-disaster' categories.",
-      banner: "/lovable-uploads/twitter.png",
-      tags: ["Machine Learning", "Deep Learning", "MLFlow", "EDA"],
-      github: "https://github.com/dnaby/NLP-Disaster-Tweets-Detection?tab=readme-ov-file",
-    },
-    {
       title: "PageRank Comparison with PySpark on GCP",
       description: "Compared PageRank with PySpark DataFrame and RDD, analyzing partitioning and cluster performance on Google Cloud Dataproc.",
       tags: ["Big Data", "PySpark", "Google Cloud Dataproc", "Distributed Computing", "PageRank", "Cluster Performance", "Data Partitioning"],
@@ -63,6 +63,14 @@ export const projects = {
       github: "https://github.com/KikiaDia/chatbot_agricole",
       videoUrl: "https://drive.google.com/file/d/1WjI0a3tap1j02RL4FG1sSCZayJK8j-Bt/preview"
     },
+        {
+      title: "Dashboard RH – 3ᵉ Trimestre 2024",
+      description: "Tableau de bord interactif en Python pour analyser les données RH du 3ᵉ trimestre 2024, consolider les KPI, visualiser les tendances mensuelles et interroger les données via un agent conversationnel.",
+      banner: "/lovable-uploads/hr_dashboard.png",
+      tags: ["Dash", "Plotly", "pandas", "LangChain", "GPT-4", "Pyngrok"],
+      github: "https://github.com/KikiaDia/Dashboard-RH.git",
+      videoUrl :"https://drive.google.com/file/d/1Qb-79WZH8c_pjQgg7Sejs5djHRzsQrsE/preview"
+    },
     {
       title: "Classification Multilabel Codeforces",
       description: "Classification multi-label de problèmes de programmation Codeforces, prédiction des tags algorithmiques via modèles ML et features extraits du code.",
@@ -71,12 +79,11 @@ export const projects = {
       github: "https://github.com/KikiaDia/multilabel_code_classification.git"
     },
     {
-      title: "Dashboard RH – 3ᵉ Trimestre 2024",
-      description: "Tableau de bord interactif en Python pour analyser les données RH du 3ᵉ trimestre 2024, consolider les KPI, visualiser les tendances mensuelles et interroger les données via un agent conversationnel.",
-      banner: "/lovable-uploads/hr_dashboard.png",
-      tags: ["Dash", "Plotly", "pandas", "LangChain", "GPT-4", "Pyngrok"],
-      github: "https://github.com/KikiaDia/Dashboard-RH.git",
-      videoUrl :"https://drive.google.com/file/d/1Qb-79WZH8c_pjQgg7Sejs5djHRzsQrsE/preview"
+      title: "Détection de Catastrophes dans les Tweets",
+      description: "Développement, optimisation et déploiement de modèle de machine learning pour classifier les tweets en catégories 'catastrophe' et 'non-catastrophe'.",
+      banner: "/lovable-uploads/twitter.png",
+      tags: ["Machine Learning", "Deep Learning", "MLFlow", "EDA"],
+      github: "https://github.com/dnaby/NLP-Disaster-Tweets-Detection?tab=readme-ov-file",
     },
     {
       title: "Détection de Fraude Fintech",
@@ -93,13 +100,6 @@ export const projects = {
       banner: "/lovable-uploads/Azure Produits et services Data _page-0025.jpg",
       tags: ["Azure", "Databricks", "Power BI", "Machine Learning"],
       github: "https://github.com/KikiaDia/Real-time-Weather-Pipeline/tree/main",
-    },
-    {
-      title: "Détection de Catastrophes dans les Tweets",
-      description: "Développement, optimisation et déploiement de modèle de machine learning pour classifier les tweets en catégories 'catastrophe' et 'non-catastrophe'.",
-      banner: "/lovable-uploads/twitter.png",
-      tags: ["Machine Learning", "Deep Learning", "MLFlow", "EDA"],
-      github: "https://github.com/dnaby/NLP-Disaster-Tweets-Detection?tab=readme-ov-file",
     },
     {
       title: "Comparaison de PageRank avec PySpark sur GCP",
